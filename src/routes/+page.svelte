000f7c3a1c04010fd7574aac97c0884edb6f57c0
zorvi <aervxa@gmail.com>
2025-11-19 17:13:03 +0300

    Create "main layer"

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="flex h-full w-full flex-col items-center-safe justify-center-safe bg-background">
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+	</p>
+</main>
