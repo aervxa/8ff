1f566c112a041a3572431cf2e18557fc4e28184b
zorvi <aervxa@gmail.com>
2025-11-27 14:44:42 +0300

    Move hints to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Result from '$lib/components/Result.svelte';
+	import * as Kbd from '$lib/components/ui/kbd';
 	import Words from '$lib/components/Words.svelte';
 	import type { TypingResults } from '$lib/utils';
 
@@ -23,4 +24,17 @@
 			}}
 		/>
 	{/if}
+
+	<!-- Hints -->
+	<div
+		class="sized-40 absolute bottom-0 left-1/2 flex -translate-x-1/2 flex-col pb-8 whitespace-nowrap [&_span]:text-sm"
+	>
+		<Kbd.Group>
+			<Kbd.Root>tab</Kbd.Root>
+			<span>+</span>
+			<Kbd.Root>enter</Kbd.Root>
+			<span>-</span>
+			<span>restart test</span>
+		</Kbd.Group>
+	</div>
 </main>
